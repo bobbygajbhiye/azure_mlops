ec80bca3791f859cf4aefdc3ad0334fc5d2ad52e
Bobby Gajbhiye <bobbygajbhiye@ymail.com>
2024-02-22 12:24:39 +0530

    Update install-requirements.sh

--- environment_setup/install-requirements.sh
+++ environment_setup/install-requirements.sh
@@ -1,5 +1,6 @@
 python --version
-pip install azure-cli
-pip install azureml-sdk[cli]
-pip install pytest
-pip install pytest-cov
+pip install --upgrade pip
+pip install azure-cli==2.57.0
+pip install azureml-sdk[cli]==1.55.0
+pip install pytest==7.4.4
+pip install pytest-cov==4.1.0
