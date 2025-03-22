85cd7fc58ebcdbef5f184b1471735b2c3de49aff
Bobby Gajbhiye <bobbygajbhiye@ymail.com>
2025-03-22 10:15:20 +0530

    updated yml file

--- environment_setup/install-requirements.sh
+++ environment_setup/install-requirements.sh
@@ -1,6 +1,10 @@
 python --version
 pip install --upgrade pip
-pip install azure-cli==2.57.0
-pip install azureml-sdk[cli]==1.55.0
-pip install pytest==7.4.4
-pip install pytest-cov==4.1.0
+#pip install azure-cli==2.57.0
+#pip install azureml-sdk[cli]==1.55.0
+#pip install pytest==7.4.4
+#pip install pytest-cov==4.1.0
+pip install azure-cli
+pip install azureml-sdk[cli]
+pip install pytest
+pip install pytest-cov
